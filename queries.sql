37a2f7330990dd3b393b562834c31c0b3d7834c2
hustle2live <kuz.v@ukr.net>
2025-03-10 22:21:17 +0200

    added db movies

---
+++ queries.sql
@@ -0,0 +1,9 @@
+INSERT INTO movie (title, about, budget, released, time_length, country_name, director_id) VALUES 
+   ('Godzilla x Kong',  'Following their explosive showdown, Godzilla and Kong must reunite against a colossal undiscovered',
+   112201200, '2021-02-04 00:00:01.000000', '141 minutes', 'Ukraine', 7),
+   ('My Spy The Eternal City',  'of toxic waste causes an underground cave dweller to mutate into a hideous monster. A construction ',
+   140200200, '2023-12-11 00:00:01.000000', '141 minutes', 'USA', 2),
+   ('Under Paris',  'Forty years after his unforgettable first case in Beverly Hills, Detroit cop Axel Foley',
+   82201200, '2024-09-04 00:00:01.000000', '141 minutes', 'Ukraine', 7),
+   ('Black Noise',  'After their car breaks down in an eerie small town, a young couple are forced to spend the night',
+   33120200, '2016-02-10 00:00:01.000000', '141 minutes', 'USA', 1);
